da85272fec34119d054e2f5a0e07695ab23cf1d7
Willy <willybc06@gmail.com>
2020-07-29 04:49:46 -0300

    1.1

---
+++ 1.1.py
@@ -0,0 +1,28 @@
+
+##Cuantas horas son 105 minutos?
+print("Cuantas horas son 105 minutos?")
+
+hora = 1
+minutos = 60
+cantidad = 105
+
+## 1 hora _____ 60 min
+## x hora _____ 105 min
+
+total1 = (hora*cantidad)/minutos
+print(total1)
+
+##----------------------------------------------
+
+
+##Cuantos kilometros son 20 millas ? (Si un kilometro corresponde a 0,6214 millas)
+print("Cuantos kilometros son 20 millas?")
+cant = 20
+kilo = 1
+millas = 0.6214
+
+## 1 kilo ______ 0,6214 millas
+## x      ______ 20 millas
+
+total2 = (kilo*cant)/millas
+print (total2)
