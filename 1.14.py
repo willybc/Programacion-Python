ec20de74740f520c093ceb450525a63b09582ab7
Willy <willybc06@gmail.com>
2020-08-03 16:47:20 -0300

    1.14

---
+++ 1.14.py
@@ -0,0 +1,14 @@
+frutas = 'Manzana,Naranja,Mandarina,Banana,Kiwi'
+
+print(frutas[0])
+
+print(frutas[1])
+
+print(frutas[2])
+
+print(frutas[-1])        # Último caracter
+
+print(frutas[-2])        # Índices negativos se cuentan desde el final
+
+new = frutas.replace('Manzana','manzana')
+print(new)
