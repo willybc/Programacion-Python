3bc806eac1dae6adc18a0238e6aefc3df16526c9
Willy <willybc06@gmail.com>
2020-08-03 17:17:07 -0300

    1.15

--- 1.14.py
+++ 1.14.py
@@ -1,14 +1,9 @@
+#Agrega 'Melón'` al principio de la cadena:
+
 frutas = 'Manzana,Naranja,Mandarina,Banana,Kiwi'
 
-print(frutas[0])
+frutas = frutas + ',' + 'Pera'
 
-print(frutas[1])
+frutas = 'Melón' + ',' + frutas 
 
-print(frutas[2])
-
-print(frutas[-1])        # Último caracter
-
-print(frutas[-2])        # Índices negativos se cuentan desde el final
-
-new = frutas.replace('Manzana','manzana')
-print(new)
+print(frutas)
