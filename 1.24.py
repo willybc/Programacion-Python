f457ecb5a7b3c77a3400ce75e0545166e57da9fc
Willy <willybc06@gmail.com>
2020-08-06 20:52:33 -0300

    1.24

---
+++ 1.24.py
@@ -0,0 +1,13 @@
+#1.24
+##Usá los operadores `in` o `not in` para verificar si `'Granada'`,`'Lima'`, y `'Limon'` pertenecen a la lista de frutas.
+
+lista_frutas = ['Frambuesa', 'Manzana', 'Granada', 'Mandarina', 'Banana', 'Pera']
+
+print('Granada' in lista_frutas)
+#print(bool(lista_frutas.index('Granada') ))
+
+print('Lima' in lista_frutas)
+
+print('Limon' not in lista_frutas)
+
+
