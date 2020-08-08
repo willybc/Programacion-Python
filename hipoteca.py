8d91c9c058f82e586804acbb2f06b676a3dc8e6a
Willy <willybc06@gmail.com>
2020-08-08 17:25:30 -0300

    1.11 hipoteca

--- hipoteca.py
+++ hipoteca.py
@@ -11,10 +11,14 @@
 #1.10
 #Modicá tu programa para que imprima una tabla mostrando el mes, el total pagado hasta el momento y el saldo restante. La salida debería verse aproximadamente así:
 
+#1.13
+#Corregir el codigo para que el pago del ultimo mes se ajuste a lo adecuado.
+
 saldo = 500000.0
 tasa = 0.05
 pago_mensual = 2684.11
 total_pagado = 0.0
+total_pagado_interes = 0.0
 
 pago_extra = 1000
 duracion_extra = 12
@@ -23,26 +27,33 @@ contador_mes = 0
 
 pago_extra_mes_comienzo = 61
 pago_extra_mes_fin = 108
-#pago_extra = 1000
+
+saldo_anterior = 0.0
 
 while saldo > 0:
     contador_mes= contador_mes + 1
 
     saldo = (saldo * (1+tasa/12) ) - pago_mensual
-
+    #condicion problema 1.8 Adelantos
     if duracion_extra > 0:
         saldo = saldo - pago_extra
         total_pagado = total_pagado + pago_extra
         duracion_extra = duracion_extra -1
 
+    #condicion 1.9 Calculadora de adelantos
     if contador_mes >= 61 and contador_mes <= 108:
         saldo = saldo -pago_extra
         total_pagado = total_pagado + pago_extra
-        
-    total_pagado = total_pagado + pago_mensual
+
+    #condicion 1.11 Bonus
+    if 0 >= saldo:
+        saldo = saldo - saldo
+        total_pagado = total_pagado + saldo_anterior 
+    else:
+        total_pagado = total_pagado + pago_mensual
+
+    saldo_anterior = saldo
     print(contador_mes, total_pagado, saldo)
+    #nota: si entra en la condicion , le sumo al total_pagado el saldo anterior con el interes incluido
 
-#print('Total pagado', total_pagado)
-#print('Meses:', contador_mes)
-
-print(f'total pagado:{total_pagado} en {contador_mes} meses')
+print(f'total pagado sin interes:{total_pagado} en {contador_mes} meses')
