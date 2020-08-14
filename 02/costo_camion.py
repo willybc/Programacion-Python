1ec3c5eaa3095284d7e80445b6c07bd6ee4ae434
Willy <willybc06@gmail.com>
2020-08-14 13:41:06 -0300

    2.5 2.6

--- 02/costo_camion.py
+++ 02/costo_camion.py
@@ -1,23 +1,22 @@
 #Abrir el archivo que lea las lineas y calcule el precio pagado por los cajones cargados en el camion
 #Ayuda para intepretar un string 's' como un numero entero, usa int(s). Para punto flotante , usa float(s)
 
-f = open('../Data/camion.csv', 'rt')
+def costo_camion(nombre_archivo):
+    #f = open('../Data/camion.csv', 'rt')
+    f = open('../Data/missing.csv', 'rt')
+    headers = next(f).split(',')
+    valores = []
+    precio_total = 0.0
 
-headers = next(f).split(',')
+    for line in f:
+        try:
+            valores = line.strip().split(",")
+            precio_pagado = float(valores[1]) * float(valores[2])
+            precio_total = precio_pagado + precio_total
+        except ValueError:
+            print(f'error en lectura de datos en fila {line}' )
+    f.close()
+    return precio_total
 
-valores= []
-precio_total = 0.0
-
-for line in f:
-    valores = line.strip().split(",")
-
-    precio_pagado = float(valores[1]) * float(valores[2])
-    precio_total = precio_pagado + precio_total
-    
-f.close()
-
-print('Costo total' ,precio_total)
-
-
-
-
+costo = costo_camion('../Data/missing.csv')
+print('Costo total:', costo)
