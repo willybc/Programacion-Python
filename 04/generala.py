1f1db931959ac78191a89e9410e8a9b0cd612c98
Willy <willybc06@gmail.com>
2020-08-30 17:42:46 -0300

    4.7 v3

--- 04/generala.py
+++ 04/generala.py
@@ -6,70 +6,77 @@ Created on Sat Aug 29 17:05:58 2020
 """
 import random
 
-def tirar():
+def tirar(tiros):
     tirada = []
-    for i in range(5):
+    for i in range(tiros):
         tirada.append(random.randint(1,6))
     return tirada
 
 def es_generala(tirada):
     cont = 0
     anterior = 0
-    lista = []
 
     for i in tirada:
         if i == anterior:
             cont+=1
-            lista.append(i)
-        else:
-            lista.append(i)
         anterior = i
-    print('tiradas', lista)
-    return lista
-'''    
+
     if cont == 4:
         return True
     else:
         return False
-'''
 
-def primer_tirada(lista):
+def primer_tirada(tirada):
     cont_rep0 = 0
     cont_rep1 = 0
     cont_rep2 = 0
     cont_rep3 = 0
     cont_rep4 = 0
-    for j in lista:
-            if lista[0] == j:
+    for j in tirada:
+            if tirada[0] == j:
                 cont_rep0+=1
-            if lista[1] == j:
+            if tirada[1] == j:
                 cont_rep1+=1
-            if lista[2] == j:
+            if tirada[2] == j:
                 cont_rep2+=1
-            if lista[3] == j:
+            if tirada[3] == j:
                 cont_rep3+=1
-            if lista[4] == j:
+            if tirada[4] == j:
                 cont_rep4+=1
     rep = [ cont_rep0, cont_rep1, cont_rep2, cont_rep3, cont_rep4 ]
     cant = max(rep)
     cont=0
     for j in rep :
         if j == cant:
-            numero = lista[cont]    
+            numero = tirada[cont]    
         cont+=1 
     return numero,cant
 
-tirada = tirar()
-lista = es_generala(tirada)
-numero, cant = primer_tirada(lista)
+tirada = tirar(5)
+es_generala(tirada)
+numero, cant = primer_tirada(tirada)
 print (numero,cant)
 
-'''
+def generala_no_servida(tirada):
+    numero,cant = primer_tirada(tirada)
+    generala = 0
+    #0 Falso / 1 True
+    if cant == 5 :
+        generala = 1
+        return generala
+    if cant != 5 :
+        for i in range(2):
+            tirada = tirar(5-cant)
+            cant += len([ k for k,j in enumerate(tirada) if j == numero ])
+            if cant == 5:
+                generala = 1
+    
+    return generala == 1
+
 N = 1000000
-salio_generala_servida = [es_generala(tirar()) for i in range(N)]
-G = sum([es_generala(tirar()) for i in range(N)])
+salio_generala_no_servida = [generala_no_servida(tirar(5)) for i in range(N)]
+G = sum([generala_no_servida(tirar(5)) for i in range(N)])
 prob = G/N
 
 print(f'Tiré {N} veces, de las cuales {G} saqué generala servida.')
 print(f'Podemos estimar la probabilidad de sacar generala servida mediante {prob:.6f}.')
-'''
