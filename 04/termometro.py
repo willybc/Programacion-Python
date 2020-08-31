f953441a60605d3428500e58b439e2409d03de99
Willy <willybc06@gmail.com>
2020-08-31 00:35:52 -0300

    4.11 termometro py

---
+++ 04/termometro.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 00:22:04 2020
+
+@author: WILLY
+"""
+
+import random
+
+temperatura = 37.5
+MU = 0
+SIGMA = 0.2
+
+N= 99
+
+temperaturas = [ random.normalvariate( MU, SIGMA) + temperatura for i in range(N) ]
+
+maxi = max(temperaturas)
+mini = min(temperaturas)
+prom = sum(temperaturas)/len(temperaturas)
+
+print(f' Maximo = {maxi}')
+print(f' Minimo = {mini}')
+print(f' Promedio = {prom}')
