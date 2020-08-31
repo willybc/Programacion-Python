05661f465cb6417cae1ac88aa5b862e7d6c2870c
Willy <willybc06@gmail.com>
2020-08-31 02:52:28 -0300

    4.13 termometro py np savetxt

---
+++ 04/4.12.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 00:59:58 2020
+
+@author: WILLY
+"""
+
+import numpy as np
+
+#4.12
+#Generar un vector que tenga los numeros impares entre el 1 y el 19 
+a = np.arange(1,20,2)       #**primer numero** **limite** **paso**
+b = np.linspace(1,19,10)    #**primer numero** **ultimo numero** **cantidad**
+
+
--- 04/termometro.py
+++ 04/termometro.py
@@ -6,14 +6,17 @@ Created on Mon Aug 31 00:22:04 2020
 """
 
 import random
+import numpy as np
 
 temperatura = 37.5
 MU = 0
 SIGMA = 0.2
 
-N= 99
+N= 999
 
 temperaturas = [ random.normalvariate( MU, SIGMA) + temperatura for i in range(N) ]
+
+np.savetxt('../Data/Temperaturas.npy', temperaturas)
 
 maxi = max(temperaturas)
 mini = min(temperaturas)
@@ -26,3 +29,4 @@ print(f' Promedio = {prom}')
 temperaturas.sort()
 posicion_central = int(((N-1)/2)+1)
 print(temperaturas[posicion_central])
+
